9a98990c896c208a635ae540397d57abae77e67c
ryanuo <1577791638@qq.com>
2025-08-10 08:24:48 +0800

    feat(meta): update namespace and version, add license

--- src/twitter-top-button/meta.ts
+++ src/twitter-top-button/meta.ts
@@ -1,12 +1,13 @@
 const meta = {
   'name': 'Twitter / X 回到顶部按钮',
-  'namespace': 'https://github.com/ryanuo/userscripts',
-  'version': '1.0.0',
+  'namespace': 'twitter-top-button',
+  'version': '1.0.1',
   'description': '在 Twitter / X 页面添加回到顶部按钮，支持快捷键 T',
   'author': 'ryanuo',
   'match': ['https://twitter.com/*', 'https://x.com/*'],
   'grant': 'none',
   'run-at': 'document-idle',
+  'license': 'apache-2.0',
   'updateURL': 'https://raw.githubusercontent.com/ryanuo/userscripts/main/scripts/twitter-top-button.user.js',
   'downloadURL': 'https://raw.githubusercontent.com/ryanuo/userscripts/main/scripts/twitter-top-button.user.js',
 }
