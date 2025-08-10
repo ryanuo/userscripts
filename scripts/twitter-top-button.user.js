b982d92c02570ef6e5b48d0292544d3499006b3e
ryanuo <1577791638@qq.com>
2025-08-10 08:28:44 +0800

    fix(twitter-top-button): update version and last modified time

--- scripts/twitter-top-button.user.js
+++ scripts/twitter-top-button.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name Twitter / X 回到顶部按钮
-// @namespace https://github.com/ryanuo/userscripts
-// @version 1.0.0
+// @namespace twitter-top-button
+// @version 1.0.1
 // @description 在 Twitter / X 页面添加回到顶部按钮，支持快捷键 T
 // @author ryanuo
 // @match https://twitter.com/*
 // @match https://x.com/*
 // @grant none
 // @run-at document-idle
+// @license apache-2.0
 // @updateURL https://raw.githubusercontent.com/ryanuo/userscripts/main/scripts/twitter-top-button.user.js
 // @downloadURL https://raw.githubusercontent.com/ryanuo/userscripts/main/scripts/twitter-top-button.user.js
 // ==/UserScript==
