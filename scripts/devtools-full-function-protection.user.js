d532d959559f288b4aaff5c7e1b4bd3a118a652b
ryanuo <66169324+ryanuo@users.noreply.github.com>
2025-08-16 07:37:23 +0000

    chore: auto build userscripts and docs [ci skip]

--- scripts/devtools-full-function-protection.user.js
+++ scripts/devtools-full-function-protection.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name DevTools 全功能防护
 // @namespace devtools-full-function-protection
-// @version 2.0
+// @version 2.0.1
 // @description 一站式解除网页的防调试、重定向、右键屏蔽，同时屏蔽 disable-devtool / devtools-detector，让 DevTools 自由使用。
 // @author ryanuo
 // @match *://*/*
 // @grant none
 // @license MIT
-// @runAt document-start
+// @run-at document-start
 // ==/UserScript==
 
 (() => {
